ea62e0e0d82e07110e6fd6f9806d79996b7850d6
chihyanglee <chihyanglee@gmail.com>
2021-07-13 02:18:05 -0400

    add 1-2 and 1-3

---
+++ chapter01/1-2.c
@@ -0,0 +1,16 @@
+/*
+ * Experiment to find out what happens when printf's argument
+ * string contains \c, where c is some character not listed above.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello, world\n");
+	printf("hello, world\c");
+	printf("hello, world\g");
+	printf("hello, world\\");
+	printf("hello, world\?");
+	printf("hello, world\123");
+}
